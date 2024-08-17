d29e2e61e55839ab0dcf6570e76a4ff1b73b45bf
Qais <nuristanyqais@gmail.com>
2024-08-17 14:26:06 +0430

    added simplejwt in the settigs

--- find_job/settings_rail.py
+++ find_job/settings_rail.py
@@ -6,7 +6,8 @@ from decouple import config
 SECRET_KEY = config('SECRET_KEY')
 
 
-ALLOWED_HOSTS = ['web-production-262c.up.railway.app']
+
+ALLOWED_HOSTS = ['web-production-262c.up.railway.app', '127.0.0.1']
 
 
 CSRF_TRUSTED_ORIGINS = [
